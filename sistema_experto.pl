37b83b0ac3b76de45540065ad113a9dcee57698e
dav96ram <d.ramirez.g@edu.utc.mx>
2021-05-04 14:05:13 -0500

    codigo completado

--- sistema_experto.pl
+++ sistema_experto.pl
@@ -74,3 +74,212 @@ imagen_portada(D, li),
  send(D,append(Bcancelar)),
  send(D,append(Boton2)),
   send(D,open_centered).
+:-inicio.
+
+%llamando a la funcion principal de nuestro boton iniciar
+
+
+principal:-
+	new(D2, dialog('SISTEMA EXPERTO UTC',size(500,400))), %inicio de nuestra interfaz emergente
+	new(Label10, label(nombre,'')),send(Label10,colour,red),%color de texto de la variable D2
+
+        imagen_portada(D2, pizarronutc), % carga de imagen
+
+
+	new(@texto,label(text,'                                            Una vez finalizado el TEST podras ver los resultados:')),
+		new(@respl,label(text,'')),
+	new(Salir,button('Salir',and(message(D2,destroy),message(D2,free)))),
+
+
+
+
+
+%creación del boton para dar inicio al TEST.
+
+	new(@boton,button('Da click si estas listo',message(@prolog,botones))),
+
+	send(D2, append(Label10)),
+	new(@btncarrera,button('¿analisis?')), %boton para iniciar el test
+	send(D2, display,Label10,point(10,20)), %mostrar el contenido de nuestra ventana
+	send(D2, display,@boton,point(310,80)), %numeros ajustan las posiciones del  boton
+	send(D2, display,@texto,point(50,40)),
+	send(D2, display,Salir,point(340,330)),
+	send(D2, display,@respl,point(90,90)),
+	send(D2,open_centered).                 %fin de la funcion
+
+
+% se muestra la respuesta de tu test que realizaste por medio de las preguntas
+
+resultado(ingenieria_de_sistemas):-ingenieria_de_sistemas,!.
+resultado(turismo):-turismo,!.
+resultado(contabilidad):-contabilidad,!.
+resultado(administracion):-administracion,!.
+resultado(diseño):-diseño,!.
+resultado(derecho):-derecho,!.
+resultado(pedagogia):-pedagogia,!.
+resultado(no_se_encontro_ninguna_carrera_adecuada_para_ti).
+
+
+%se declran las variables de cada carrera y despues se colocan las preguntas coreespodientes
+
+
+ingenieria_de_sistemas:-carrera_ingenieria_de_sistemas,
+	consultar('¿Eres hábil resolviendo problemas?'),
+	consultar('¿Te gusta resolver problemas matemáticos?'),
+	consultar('¿te interesan temas sobre tecnologia?'),
+	consultar('¿Tienes facilidad para ocupar computadoras?'),
+	consultar('¿Te gusta crear cosas?').
+
+turismo:-carrera_turismo,
+	consultar('¿Tienes interes por otras culturas?'),
+	consultar('¿tienes gusto y facilidad por los idiomas?'),
+	consultar('¿Te gusta viajar?'),
+	consultar('¿Tienes gusto por el cuidado al patrimonio cultural?'),
+	consultar('¿Se te facilita adaptarte al ambiente cultural internacional?').
+
+contabilidad:-carrera_contabilidad,
+	consultar('¿Te gustaria trabajar en un Banco?'),
+	consultar('¿Te gusta contar dinero?'),
+	consultar('¿Te apasionan los negocios?'),
+        consultar('¿Eres buen negociador?'),
+        consultar('¿Tienes habilidad con los numeros y sacas cuenta con facilidad?').
+
+administracion:-carrera_administracion,
+	consultar('¿Eres bueno en la comunicacion continua?'),
+	consultar('¿Eres bueno en el manejo de tiempos?'),
+	consultar('¿Eres bueno para el pensamiento estrategico?'),
+	consultar('¿Tienes capacidad de planificar?'),
+	consultar('¿Te interesaria aprender ingles?').
+
+diseño:-carrera_diseño,
+         consultar('¿Eres creativo?'),
+	consultar('¿Eres bueno dibujando?'),
+	consultar('¿Te gusta diseñar arquitecturas artisticas?'),
+	consultar('¿Eres bueno con software de edicion?'),
+	consultar('¿Te gustan las pinturas?').
+
+derecho:-carrera_derecho,
+       consultar('¿Tienes facilidad para argumentar?'),
+       consultar('¿Tienes facilidad para la expresion oral?'),
+       consultar('¿Tienes pasion por debatir?'),
+       consultar('¿Tiene capacidad para lidiar con la incertidumbre?'),
+       consultar('¿Te gusta la historia?').
+
+pedagogia:-carrera_pedagogia,
+       consultar('¿Te gusta el aprendizaje didactico?'),
+       consultar('¿Te agrada las actividades recreativas?'),
+       consultar('¿Te gustaria crear modelos educativos que mejore la enseñanza de los profesores?'),
+       consultar('¿Te gustaria diseñar material didactico?'),
+       consultar('¿te interesa la psicologia educativa?').
+
+
+no_se_encontro_ninguna_carrera_adecuada_para_ti:-no_hay_una_carrera.
+
+
+%funcion de corte cada vez que se anula una pregunta de la carrera posteriormente mandara hacia la siguiente
+
+carrera_ingenieria_de_sistemas:-consultar('¿Eres hábil resolviendo problemas?'),!.
+carrera_turismo:-consultar('¿Tienes interes por otras culturas?'),!.
+carrera_contabilidad:-consultar('¿Te gustaria trabajar en un Banco?'),!.
+carrera_administracion:-consultar('¿Eres bueno en la comunicacion continua?'),!.
+carrera_diseño:-consultar('¿Eres creativo?'),!.
+carrera_derecho:-consultar('¿Tienes facilidad para argumentar?'),!.
+carrera_pedagogia:-consultar('¿Te gusta el aprendizaje didactico?'),!.
+
+
+
+
+:-dynamic si/1,no/1. %suceptible a generar cambios entre verdadero o falso
+
+preguntar(Problema):-new(Di,dialog('TEST VOCACIONAL')),
+	new(L9,label(texto,'Responde las siguientes preguntas')),
+	new(L10,label(text,Problema)),
+
+% crear botones para si y no
+
+
+	new(Z1,button(si,and(message(Di,return,si)))),
+	new(Z2,button(no,and(message(Di,return,no)))),
+	send(Di,gap,size(25,25)),
+
+% llamada de los botones y ventanas emergentes
+
+	send(Di,append(L9)),
+	send(Di,append(L10)),
+	send(Di,append(Z1)),
+	send(Di,append(Z2)),
+
+	send(Di,default_button,si),
+	send(Di,open_centered), get(Di,confirm,Answer),
+	write(Answer), send(Di,destroy),
+
+%confirmacion de respuesta si y no
+
+ (   (Answer==si)->assert(si(Problema)); %confirma las preguntas si es si en confirmacion y si es no erroneo
+ assert(no(Problema)),fail).
+
+%cada vez que el usuario contesta una pregunta la pantalla se limpria y se vuelve a preguntar
+consultar(S):-(si(S)->true; (no(S)->fail; preguntar(S))).
+limpiar:-retract(si(_)),fail.
+limpiar:-retract(no(_)),fail.
+limpiar.
+
+% borrado de la ventana emergente para dar el resultado final
+botones:-borrado,
+	send(@boton,free),
+	send(@btncarrera,free),
+
+% resultado de la carrera elegida
+
+resultado(Carrera),
+	send(@texto, selection('                            Hemos analizado sus respuestas y la carrera mas adecuada para ti es:')),
+	send(@respl,selection(Carrera)),
+	new(@boton, button('Iniciar su evaluacion',message(@prolog, botones))),
+	send(Menu, display,@boton,point(40,600)),       %llamando los resultados de la ventana emergente
+	send(Menu, display,@btncarrera,point(20,50)),
+	send(Menu, append, new(Ayuda, popup(Ayuda))),
+	limpiar.
+
+borrado:-send(@respl,selection('')). %fin del programa
+
+%boton de textos para saber la guia basica
+main1:-
+	new(D3, dialog('SISTEMA EXPERTO UTC',size(500,400))),
+	new(Label10, label(nombre,'')),send(Label10,colour,red),
+
+        imagen_portada(D3, pizarronutc),
+
+
+	new(@texto,label(text,'                                      Guia basica para saber el guncionamiento de la pagina de prolog:')),
+         new(@texto1,label(text,'Este sistema está diseñado para interactuar e informar al usuario que desee saber información sobre la ')),
+	 new(@texto2,label(text,'Universidad Tres Culturas, además de dar una orientación sobre las carreras que se imparten en la')),
+         new(@texto3,label(text,'institución tomando como base la toma de decisiones con respecto a los datos ingresados.')),
+	  new(@texto4,label(text,'Da clic en inicio y responde las preguntas que te aparecen en la interfaz')),
+          new(@texto5,label(text,'Contesta las preguntas de acuerdo con las habilidades y aptitudes que consideres tengas y pongas ')),
+          new(@texto6,label(text,'en práctica.')),
+          new(@texto7,label(text,'Después se te mostraran tus resultados de acuerdo a la carrera que consideremos más apta para ti')),
+          new(@texto8,label(text,'y después presión salir. ®')),
+		new(@respl,label(text,'')),
+	new(Salir,button('Salir',and(message(D3,destroy),message(D3,free)))),
+
+
+
+
+
+
+
+
+	send(D3, append(Label10)),
+	send(D3, display,Label10,point(10,20)),
+	send(D3, display,@texto,point(50,40)),
+        send(D3, display,@texto1,point(70,70)),
+        send(D3, display,@texto2,point(78,82)),
+        send(D3, display,@texto3,point(88,94)),
+        send(D3, display,@texto4,point(150,150)),
+        send(D3, display,@texto5,point(85,200)),
+        send(D3, display,@texto6,point(85,214)),
+        send(D3, display,@texto7,point(85,230)),
+        send(D3, display,@texto8,point(85,245)),
+	send(D3, display,Salir,point(400,400)),
+	send(D3, display,@respl,point(90,90)),
+	send(D3,open_centered).
